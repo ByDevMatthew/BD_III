a9410d52f6ee442873d888d5d152fb4d63f0e9a9
Matheus Tavares <matheustavares1429@gmail.com>
2026-03-22 13:11:33 -0300

    Add usuarios table with sample data and query
    Created a new table 'usuarios' with sample data and a query.

--- select.sql
+++ select.sql
@@ -1,1 +1,12 @@
-SELECT * FROM table_original_name;
+-- Create tables
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    email VARCHAR(100)
+);
+
+-- Insert data
+INSERT INTO usuarios (nome, email) VALUES ('Matheus', 'matheus@email.com');
+
+-- Queries
+SELECT * FROM usuarios;
